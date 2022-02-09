4b5dd02cafa526d0acddf445ee563c4af4e99c41
Leonel <juizleonel@gmail.com>
2022-02-08 23:38:18 -0300

    Revert "Mejoras en general"
    This reverts commit 5162eb3d1c1db6f069b2fc99d8fc419d5ca80768.

--- javascript/ahorcado.js
+++ javascript/ahorcado.js
@@ -1,72 +1,23 @@
 var botonJugar = document.getElementById("iniciar-juego");
 var poolPalabras = ["XILOFON", "ORNITORRINCO", "PARALELEPIPEDO", "CENTURION", "ROMANI", "ARAUCARIA", "MURCIELAGO", "DANTESCO", "ARTILLERIA"];
-var poolLetrasEquivocadas = [];
-var letraActual = "";
-var palabraDeletreada = [];
 
 botonJugar.addEventListener("click",function(event){
     event.preventDefault();
     window.scroll(0,800);
-    
     var numeroOrden = Math.floor(Math.random()*(poolPalabras.length-0));
     var palabraSorteada = poolPalabras[numeroOrden];
-    console.log(palabraSorteada);
-    palabraDeletreada = palabraSorteada.split('');
+    var palabraDeletreada = palabraSorteada.split('');
     var numeroLetras = palabraDeletreada.length;
     
-    dibujarGuiones (numeroLetras);
-    dibujarHorca();
-
+    return numeroLetras;
 });
 
-function dibujarGuiones (numeroLetras) {
-
-    for (var i = 0; i < numeroLetras; i++) {
-        var offset = 430 + (45 * i);
-        dibujarLinea(offset);
-    }
-}
-
-document.addEventListener('keydown',(event) => {
-    letraActual = event.key.toUpperCase(); 
-    console.log(letraActual);
-    var posiciones = [];
-    posiciones = estaEn();
-//    console.log(posiciones);
-    if (posiciones.length == 0){
-        agregarLetraEquivocada();
-    }
+/*
+function jugar(palabraDeletreada){
     
-});
-
-function estaEn (){
-    var posiciones = [];
-    for (var i = 0; i < palabraDeletreada.length; i++){
-        if (letraActual == palabraDeletreada[i]) {
-            posiciones.push(i);
-//            console.log(posiciones)
-        }
-    }
+    var letraPropuesta =
+    var letrasIndicadas = [];
     
-    return posiciones;
-}
+    for
 
-function agregarLetraEquivocada () {
-    var letraRepetida = false;
-    if (poolLetrasEquivocadas.length == 0){
-        poolLetrasEquivocadas.push(letraActual);
-    }else{
-        poolLetrasEquivocadas.forEach((value) =>{
-            console.log(value);
-            if (letraActual == value){
-                letraRepetida = true;
-            }
-        });
-        if(letraRepetida){
-            console.log("Error");
-        } else {
-            poolLetrasEquivocadas.push(letraActual);
-        }
-    }
-    console.log(poolLetrasEquivocadas);
-}
+} */
--- javascript/canvas.js
+++ javascript/canvas.js
@@ -1,84 +1,69 @@
 let pantalla = document.querySelector("canvas");
 let pincel = pantalla.getContext("2d");
-var intentos = 6;
+let intentos = 6;
+
+//Triángulo
+pincel.fillStyle = "black";
+pincel.beginPath();
+pincel.moveTo(100,800);
+pincel.lineTo(150,750);
+pincel.lineTo(200,800);
+pincel.fill();
+
+//palo principal
+pincel.fillStyle = "black";
+pincel.lineWidth = 10;
+pincel.moveTo(150,750);
+pincel.lineTo(150,350);
+pincel.stroke();
+
+//palo secundario
+pincel.fillStyle = "black";
+pincel.lineWidth = 10;
+pincel.moveTo(145,350);
+pincel.lineTo(350,350);
+pincel.stroke();
+
+//Soga
+pincel.fillStyle = "black";
+pincel.lineWidth = 3;
+pincel.moveTo(340,350);
+pincel.lineTo(340,400);
+pincel.stroke();
 
 
-function dibujarHorca(){
+//Cabeza
+pincel.fillStyle = "black";
+pincel.beginPath();
+pincel.arc(340,450,50,0,Math.PI*2,true);
+pincel.moveTo(375,450);
+pincel.arc(340,450,35,0,Math.PI,false);
+pincel.moveTo(330,440);
+pincel.arc(325,440,5,0,Math.PI*2,true);
+pincel.moveTo(360,440);
+pincel.arc(355,440,5,0,Math.PI*2,true);
+pincel.stroke();
 
-    //Triángulo
-    pincel.fillStyle = "black";
-    pincel.beginPath();
-    pincel.moveTo(100,800);
-    pincel.lineTo(150,750);
-    pincel.lineTo(200,800);
-    pincel.fill();
-    
-    //palo principal
-    pincel.fillStyle = "black";
-    pincel.lineWidth = 10;
-    pincel.moveTo(150,750);
-    pincel.lineTo(150,350);
-    pincel.stroke();
-    
-    //palo secundario
-    pincel.fillStyle = "black";
-    pincel.lineWidth = 10;
-    pincel.moveTo(145,350);
-    pincel.lineTo(350,350);
-    pincel.stroke();
-    
-    //Soga
-    pincel.fillStyle = "black";
-    pincel.lineWidth = 3;
-    pincel.moveTo(340,350);
-    pincel.lineTo(340,400);
-    pincel.stroke();
-}
+//cuerpo
+pincel.fillStyle = "black";
+pincel.lineWidth = 4;
+pincel.moveTo(340,500);
+pincel.lineTo(340,650);
+pincel.stroke();
 
-if (intentos <6) {
-    //Cabeza
-    pincel.fillStyle = "black";
-    pincel.beginPath();
-    pincel.arc(340,450,50,0,Math.PI*2,true);
-    pincel.moveTo(375,450);
-    pincel.arc(340,450,35,0,Math.PI,false);
-    pincel.moveTo(330,440);
-    pincel.arc(325,440,5,0,Math.PI*2,true);
-    pincel.moveTo(360,440);
-    pincel.arc(355,440,5,0,Math.PI*2,true);
-    pincel.stroke();
+//brazo izquierdo
+pincel.fillStyle = "black";
+pincel.lineWidth = 4;
+pincel.moveTo(340,550);
+pincel.lineTo(265,475);
+pincel.stroke();
 
-}
-
-if (intentos < 5) {
-    
-    //cuerpo
-    pincel.fillStyle = "black";
-    pincel.lineWidth = 4;
-    pincel.moveTo(340,500);
-    pincel.lineTo(340,650);
-    pincel.stroke();
-}
-
-if (intentos < 4) {
-    
-    //brazo izquierdo
-    pincel.fillStyle = "black";
-    pincel.lineWidth = 4;
-    pincel.moveTo(340,550);
-    pincel.lineTo(265,475);
-    pincel.stroke();
-}
-
-if (intentos < 3) {
-    
-    //brazo derecho
-    pincel.fillStyle = "black";
-    pincel.lineWidth = 4;
-    pincel.moveTo(340,550);
-    pincel.lineTo(415,475);
-    pincel.stroke();
-}
+//brazo derecho
+pincel.fillStyle = "black";
+pincel.lineWidth = 4;
+pincel.moveTo(340,550);
+pincel.lineTo(415,475);
+pincel.stroke();
 
 //pierna derecha
 pincel.fillStyle = "black";
@@ -94,7 +79,11 @@ pincel.moveTo(340,650);
 pincel.lineTo(265,725);
 pincel.stroke();
 
-var separacionLinea = 430 
+for (var i= 0; i < numeroLetras; i++){
+    var separacionLinea = 450 + (i*60);
+    
+    dibujarLinea(separacionLinea);
+}
 
 function dibujarLinea(separacionLinea){
         pincel.fillStyle = "black";
